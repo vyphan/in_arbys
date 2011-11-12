bbf37ca71f9a2781de1c7ae008817d8d12630f0a
Jim Gourgoutis <jimg@yojimg.net>
2011-11-11 19:45:55 -0800

    documentation and slight code cleanup

--- public/scripts/libraries/athena-0.0.1.js
+++ public/scripts/libraries/athena-0.0.1.js
@@ -1,15 +1,26 @@
+/**
+ * Athena Framework Library
+ * @class athena-0.0.1
+ * @module
+ * @static
+ */
 ( function( $ ){
 
-  var pattern = pattern = '[data-athena*="ui:"]',
+  var pattern = '[data-athena*="ui:"]',
     queues = {},
     actions = {},
     required = [];
 
-  //Queue a node for execution once associated package is ready
+
+  /**
+   * Queue a node for execution once associated package is ready
+   * @method Name
+   * @public|private
+   * @param {String} key Key for package
+   * @param {Object} $node The JQuery node associated with the UI Component
+   */
   function enqueue( key, $node ) {
-    if( $node.data().queued ) {
-      return;
-    } else {
+    if ( !( $node.data().queued ) ) {
       if( queues[key] ) {
         queues[key].push( $node );
       } else {
@@ -19,12 +30,23 @@
     }
   }
 
-  //Parse node for UI components, load and instantiate, neccessary packages.
+  /**
+   * JQuery plug-in used to parse node for UI components, load and instantiate, neccessary packages.
+   * @method act
+   * @public
+   */
   $.fn.act = function() {
     var $this = $( this ),
       count;
 
-    //Instantiate a node's role.
+    /**
+     * Instantiate a node's role.
+     * @method execute
+     * @private
+     * @param {Object} $node JQuery node reference
+     * @param {String} role An event type (?)
+     * @param {Object} Action The name of the Event's class
+     */
     function execute( $node, role, Action ) {
       new Action( $node, new Function ( '$this', 'return ' + $node.data( 'athenaConfig' ) + '[\'' + role + '\']'  )( $node ) );
       $node.data( 'acting', true );
@@ -35,12 +57,17 @@
       }
     }
 
-    //Keep track of total actors found, so we can trigger an acting event when they're instanciated
+    //Keep track of total actors found, so we can trigger an acting event when they're instantiated
     count = _.reject( $( pattern, $this ), function( item, index ) {
       return $( item ).data().acting;
     } ).length;
 
-    //Recurse over children to make sure UI components are instanciated inside out. 
+    /**
+     * Recurse over children to make sure UI components are instantiated inside out. 
+     * @method recurse
+     * @private
+     * @param {Object} $this JQuery object reference pointing to the original node.
+     */
     ( function recurse( $this ) {
       var $actors,
         roles;
@@ -55,7 +82,7 @@
         return $( item ).data().acting;
       } );
 
-      //There are no child UI components, so it's safe to instanceiate it, or queue it up and load nessasary packages
+      //There are no child UI components, so it's safe to instantiate it, or queue it up and load neccessary packages
       if( $actors.length === 0 ) {
         if( $this.is( pattern ) ) {
           roles = _.reject( ( $this.data().athena || '' ).split( ' ' ), function( role, index ) {
@@ -94,12 +121,17 @@
     } ( $this ) );
 
   };
-
 }( jQuery ) );
 
-( function( $ ){
 
-  //Returns a control.
+/**
+ * JQuery plug-in to get access to a component instance bound to 
+ * the specified DOM node.
+ * @method getControl
+ * @public
+ * @param {String} id The id of the component's DOM node.
+ */
+( function( $ ){
   $.fn.getControl = function( id ) {
     return $( this ).data( id );
   };
@@ -107,7 +139,11 @@
 }( jQuery ) );
 
 
-//Hijacks $jQuery's event model so we can put in Athena speciffic hooks.
+/**
+ * Hijacks $jQuery's event model so we can put in Athena specific hooks.
+ * @class
+ * @static
+ */
 
 ( function( $ ){
 
@@ -117,6 +153,11 @@
     trigger: $.fn.trigger
   };
 
+  /**
+   * Facade for JQuery's on method
+   * @method on
+   * @public
+   */
   $.fn.on = function() {
     var $this = $( this );
     if( $this.data( 'acting' ) ) {
@@ -129,7 +170,12 @@
     }
     return cache.on.apply( $this, arguments );
   };
-  
+
+  /**
+   * Facade for JQuery's off method
+   * @method off
+   * @public
+   */  
   $.fn.off = function() {
     var $this = $( this );
     if( $this.data( 'acting' ) ) {
@@ -137,7 +183,12 @@
     }
     return cache.off.apply( $this, arguments );
   };
-  
+
+  /**
+   * Facade for JQuery's trigger method
+   * @method trigger
+   * @public
+   */  
   $.fn.trigger = function() {
     var $this = $( this );
     if( $this.data( 'athena' ) ) {
