4c4f31763ef11d8b98cbcf55af6fba139b3f83e4
Peter Frueh <pfrueh@linkedin.com>
2011-12-12 12:34:20 -0800

    updated AthenaUIFramework

---
+++ public/scripts/libraries/helpers.js
@@ -0,0 +1,37 @@
+_.explodeURL = function ( url ) {
+  var regexp = /^(([^:\/\?#]+):)?(\/\/([^\/\?#]*))?([^\.\?#]*)(\.([^\?#]*))?(\?([^#]*))?(#(.*))?/,
+    exploded = regexp.exec( url ),
+    urlFragments = {
+      scheme: exploded[2],
+      authority: exploded[4],
+      path: exploded[5],
+      extension: exploded[7],
+      query: {},
+      fragment: exploded[11]
+    },
+    queryPieces = exploded[9],
+    i,
+    j,
+    chunk;
+
+  if ( queryPieces ) {
+
+    queryPieces = queryPieces.split( '&' );
+
+    for ( j = queryPieces.length; i < j; i += 1 ) {
+      chunk = queryPieces[i].split( '=' );
+      urlFragments.query[chunk[0]] = chunk[1];
+    }
+  }
+
+
+  return urlFragments;
+
+};
+
+_.log = function () {
+  var args = arguments;
+  if ( window.ATHENA_CONFIG && window.ATHENA_CONFIG.debug && console && !!console.log ) {
+    console.log( "ATHENA: ", args );
+  }
+};
